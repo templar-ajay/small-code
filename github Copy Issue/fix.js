2e72533be214ba08f4aa6c495e25067eecd0a314
templar-command0 <templar.command0@gmail.com>
2022-09-23 17:14:32 +0530

    xx

--- github Copy Issue/fix.js
+++ github Copy Issue/fix.js
@@ -1,10 +1,12 @@
 document
   .querySelector('[data-test-selector="remote-clipboard-copy"]')
   .addEventListener("click", f);
-function f() {
-  navigator.clipboard.writeText(
-    Array.from(document.querySelector("tbody").querySelectorAll("td"))
-      .map((td, i) => (i % 2 ? td.innerText : "\n"))
-      .join("")
-  );
+async function f() {
+  const x = Array.from(document.querySelector("tbody").querySelectorAll("td"))
+    .map((td, i) => (i % 2 ? td.innerText : "\n"))
+    .join("");
+  setTimeout(() => {
+    window.navigator.clipboard.writeText(x);
+    console.log(`code copied to clipboard`);
+  }, 500);
 }
